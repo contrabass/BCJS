03c939f5d55038cad15ba631f3fe4d57bce2544d
Ma'ayan Beider-Jacobssohn <contrabass@gmail.com>
2021-04-12 01:13:52 +0300

    W2D2 class ex and misc

--- JS/W2/D1/Class Exercises/3/index.js
+++ JS/W2/D1/Class Exercises/3/index.js
@@ -1,6 +1,6 @@
 
 // 1. Create a numerically indexed table (ie. an array): pets, like this ['cat','dog','fish','rabbit','cow']
-pets = ['cat','dog','fish','rabbit','cow'];
+let pets = ['cat','dog','fish','rabbit','cow'];
 
 // 2. Display dog 
 console.log(pets[1]);
---
+++ JS/W2/D2/Class ex/1.js
@@ -0,0 +1,15 @@
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+let user = {
+    username: "Wilhelm_Furtwangler",
+    password: "KlempererSomeVerbalPejorative",
+}
+// 2. Create an array which contains the object you have made above and name the array "database".
+let database=[user];
+console.log(database);
+database[0];
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+let newsfeed = [
+    {username:'a', timeline:1},
+    {username:'b', timeline:2},
+    {username:'c', timeline:3}
+]
