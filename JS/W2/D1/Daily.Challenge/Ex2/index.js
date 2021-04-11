a39125e3fb9048d75b7e2345cbfa21cd9df601e6
Ma'ayan Beider-Jacobssohn <contrabass@gmail.com>
2021-04-11 08:09:18 +0300

    W2D1 Daily Challange(s. two of them).

---
+++ JS/W2/D1/Daily.Challenge/Ex2/index.js
@@ -0,0 +1,10 @@
+/* Exercise 2:
+Using this array :*/ 
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+//Access and then console.log “Oranges”.
+let x=moreFruits[1][1];
+console.log(x);
+
+// or directly:
+// console.log(moreFruits[1][1])
