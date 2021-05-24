814cda1525b140d4f9186bfee407e26a0b30ed57
Ma'ayan Beider-Jacobssohn <contrabass@gmail.com>
2021-05-24 09:17:44 +0300

    .

---
+++ JS/W7-NodeJS/D1/ex/4/npm-node/dist/main.dev.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var a = 5;
+var b = 10;
+console.log("Hello you are ".concat(a + b));
---
+++ JS/W7-NodeJS/D1/ex/4/npm-node/main.js
@@ -0,0 +1,11 @@
+const nm = require('nodemon');
+
+let a = 5;
+let b = 10;
+let x;
+let c = 6;
+x = a + b;
+
+console.log("First Run: " +(x));
+//added after first run
+console.log("Secont Run: " +(x+c));
